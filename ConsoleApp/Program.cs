e16c371c9208edbd62c0cae95a85bae176abbf45
HuaJiKing402 <huajiking402@outlook.com>
2023-09-24 19:22:07 +0800

    update

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TheGenesis.Core.Utils;
+
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SysUtils.SystemArch);
+        }
+    }
+}
